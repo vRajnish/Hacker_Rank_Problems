7e00e7e100b7dacecd073623b44610b3e80b84d5
Andrei Vasilev <andreisvasilev@gmail.com>
2017-02-01 21:56:59 -0800

    Create OptimizedSolution.cpp

---
+++ Pairs/OptimizedSolution.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<unordered_set>
+using std::cout; using std::cin;
+
+int main() {
+    std::unordered_set<int> compliments;
+    int size, difference, number, pairs;
+    
+    cin >> size >> difference;
+    
+    for (int i = 0; i < size; i++) {
+        cin >> number;
+        compliments.insert(number);
+    }
+    
+    for (auto it = compliments.begin(); it != compliments.end(); ++it) {
+        if (compliments.find(*it + difference) != compliments.end()) 
+            pairs++;
+    }
+    cout << pairs;
+    return 0;
+}
